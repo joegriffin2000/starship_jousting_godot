68d0328dd9671bf31f22695c12edae9803851af5
BenOwO <82422231+ZackoRYG@users.noreply.github.com>
2024-10-11 13:49:34 -0500

    Reduce dash time to 0.3 and disable steering while dashing

--- logic/ship.gd
+++ logic/ship.gd
@@ -3,15 +3,15 @@ extends CharacterBody2D
 @export var speed = 300
 @export var rotation_speed = 3
 @export var dash_speed = 900
-@export var dash_length = 1
+@export var dash_length = 0.3
 
 @onready var dash = $Dash
 var rotation_direction = 0
 
 func get_input():
-	rotation_direction = Input.get_axis("left", "right")
 	if Input.is_action_pressed("dash"):
 		dash.start_dash(dash_length)
+	rotation_direction = 0 if dash.is_dashing() else Input.get_axis("left", "right")
 	velocity = transform.y * -Input.get_action_strength("up") * dash_speed if dash.is_dashing() else transform.y * -Input.get_action_strength("up") * speed
 
 func _physics_process(delta):
