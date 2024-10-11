b5f63d0deed2bfab7d0b1fb8fb1f6dbf216daffe
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-10-11 12:15:49 -0500

    Created Dev Branch
    - Created Dev branch
    - Added some initial dash code

--- logic/ship.gd
+++ logic/ship.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var rotation_speed = 4
+@export var rotation_speed = 3
 
 var rotation_direction = 0
 
@@ -10,6 +10,11 @@ func get_input():
 	velocity = transform.y * -Input.get_action_strength("up") * speed
 
 func _physics_process(delta):
+	if Input.is_action_pressed("dash"):
+		dash()
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+
+func dash():
+	pass
