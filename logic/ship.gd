32e01ffed9288bd6b786a7ade3b7d0a51ff5fe54
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2024-10-11 13:15:08 -0500

    Finished Initial Dash Functionality
    - Created Dash Scene
    - Created dashtimer timer
    - Updated Player Speed to be 300 (goes up to 900 while dashing)
    - Made Dash last 1 second
    -

--- logic/ship.gd
+++ logic/ship.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 300
 @export var rotation_speed = 3
+@export var dash_speed = 900
+@export var dash_length = 1
 
+@onready var dash = $Dash
 var rotation_direction = 0
 
 func get_input():
 	rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.y * -Input.get_action_strength("up") * speed
+	if Input.is_action_pressed("dash"):
+		dash.start_dash(dash_length)
+	velocity = transform.y * -Input.get_action_strength("up") * dash_speed if dash.is_dashing() else transform.y * -Input.get_action_strength("up") * speed
 
 func _physics_process(delta):
-	if Input.is_action_pressed("dash"):
-		dash()
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
-
-func dash():
-	pass
