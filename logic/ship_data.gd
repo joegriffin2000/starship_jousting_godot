49de99ae25c16bdcf914348b97f57593ac992bf1
BenOwO <82422231+ZackoRYG@users.noreply.github.com>
2024-10-25 10:33:21 -0500

    restructure files

--- logic/ship_data.gd
+++ logic/ship_data.gd
@@ -1,5 +1,6 @@
 extends Node
 
+#Ship stats
 @export var speed = 300
 @export var rotation_speed = 3
 @export var dash_speed = 900
@@ -7,5 +8,9 @@ extends Node
 @export var dash_cd = 0.5
 @export var knock_back_time = 0.3
 
+#Ship states
 @export var knockback = false
 @export var dash = false
+
+#Ship resources
+@export var credits = 0
