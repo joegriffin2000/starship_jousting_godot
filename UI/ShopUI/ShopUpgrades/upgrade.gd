155f8bef0686b8e356ed91cdc1b20bb70c7e6c68
Joseph Griffin <71573789+joegriffin2000@users.noreply.github.com>
2025-01-27 14:44:23 -0600

    Expanding on original upgrade structure
    adding Upgrade json file + integration.
    remaking upgrade tree population method.
    removed previous speed boosting easter egg when selecting any upgrade.

--- UI/ShopUI/ShopUpgrades/upgrade.gd
+++ UI/ShopUI/ShopUpgrades/upgrade.gd
@@ -2,22 +2,41 @@ extends Draggable
 
 var e_button = preload("res://UI/upgrade_button.tscn")
 
+@onready var file = FileAccess.open("res://upgrades.json",FileAccess.READ).get_as_text()
+@onready var json = JSON.new()
+@onready var filecontents = json.parse_string(file)
+
 func _ready() -> void:
 	super()
-	#var button = e_button.instantiate()
-	populate()
-	
-func populate():
 	var baseButton = get_node("BaseButton")
-	for i in range(4):
-		var button = e_button.instantiate()
-		#var button = UpgradeButton.new()
-		button.set_upgrade_stat_increase("speed", 100, 3, 10)
-		add_button(baseButton, button, i)
-		
+	populate(baseButton,0)
+	
+func populate(parent_button,parent_id):
+	for i in filecontents:
+		if i["parent_id"] == parent_id:
+			var button = e_button.instantiate()
+			if i["isStatBoost"] == true:
+				button.set_upgrade_stat_increase(i["data"]["stat"], i["data"]["increase_value"], i["max_purchases"], i["cost"])
+			add_button(parent_button, button, i["id"])
+	
+# Keeping this around just in case.
+#func populate():
+	#var baseButton = get_node("BaseButton")
+	#for i in range(3):
+		#var button = e_button.instantiate()
+		##var button = UpgradeButton.new()
+		#button.set_upgrade_stat_increase("speed", 100, 3, 10)
+		#add_button(baseButton, button, i)
+		#
+	#var button = e_button.instantiate()
+	#button.set_upgrade_stat_increase("health", 1, 1, 20)
+	#add_button(baseButton, button, 3)
+
+	
 func reset():
 	get_tree().call_group("upgradeButtons","reset")
-	populate()
+	var baseButton = get_node("BaseButton")
+	populate(baseButton,0)
 		
 func add_button(parent_button, button_to_add, index: int):
 	#if parent_button.connect_count >= parent_button.max_connect_count:
